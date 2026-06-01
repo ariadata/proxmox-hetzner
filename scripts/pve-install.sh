7aec5c08975311ab1a2393cddbb2e4bc2486f45c
Sebastiano Pratellini <seba@studioboost.it>
2026-06-01 10:40:15 +0200

    wrong default

--- scripts/pve-install.sh
+++ scripts/pve-install.sh
@@ -70,7 +70,7 @@ get_system_inputs() {
     read -e -p "Enter your email address: " -i "admin@example.com" EMAIL
     read -e -p "Enter your private subnet : " -i "192.168.26.0/24" PRIVATE_SUBNET
     read -e -p "Enter your System New root password: " NEW_ROOT_PASSWORD
-    read -e -p "Enter your Country: " -i "en" COUNTRY
+    read -e -p "Enter your Country: " -i "us" COUNTRY
     read -e -p "Enter your Keyboard layout: " -i "en-us" KEYBOARD
     read -e -p "Enter the name of first disk: " -i "nvme0n1" FIRST_DISK
     read -e -p "Enter the name of second disk: " -i "nvme1n1" SECOND_DISK
